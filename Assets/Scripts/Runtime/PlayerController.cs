f0d87e9c1aa2b3d6e238bc2b1a29096be3e84e65
Exyde <54008677+Exyde@users.noreply.github.com>
2022-05-26 16:28:20 +0200

    Bloom & Feel

--- Assets/Scripts/Runtime/PlayerController.cs
+++ Assets/Scripts/Runtime/PlayerController.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
 	
+	
+	#region Fields
+	[Header ("Player Datas")]
+	[SerializeField] [Range (0, 100)] float _moveSpeed;
+
+
+	private Rigidbody _rb;
+	#endregion 
+
+	#region Unity Callbacks
+	private void Awake()
+	{
+		_rb = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		_rb.MovePosition (transform.position + transform.forward * _moveSpeed * Time.deltaTime);
+	}
+	#endregion 
+
+
+	#region Inputs Callbacks
 	public void HandleMove(Vector2 inputs) {
 		Debug.Log("Move");
 	}
 
 	public void HandleDash() => Debug.Log("Dash");
 	public void HandleInteract() => Debug.Log("Interact");
+	#endregion 
 
 }
