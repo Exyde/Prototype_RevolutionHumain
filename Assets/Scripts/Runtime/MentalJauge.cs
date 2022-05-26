316213df82c6001e9f4234097f7907bb4eab8345
Exyde <54008677+Exyde@users.noreply.github.com>
2022-05-26 23:28:51 +0200

    Basic Jauge Decay + Display

--- Assets/Scripts/Runtime/MentalJauge.cs
+++ Assets/Scripts/Runtime/MentalJauge.cs
@@ -7,19 +7,39 @@ public class MentalJauge : MonoBehaviour
 {
     [Header ("Gameplay Data")]
     [SerializeField] [Range (0, 100)] float _mentalSanity;
+    [SerializeField] int _gameDurationInSeconds = 200;
+    float _percentLossPerSecond;
 
     [Header ("References")]
     [SerializeField] GameObject _panelSanity;
+    [SerializeField] Image _imgPanelSanity;
     [SerializeField] Image _imgSanityJauge;
-
 
     void Start()
     {
+        // _panelSanity.GetComponent<Image>();
+
         _mentalSanity = 100f;
+        _percentLossPerSecond = _mentalSanity / (float)_gameDurationInSeconds;
+        InvokeRepeating("DecaySanity", 0, 1);
+        InvokeRepeating("UpdateDisplay", 0, 1);
+
     }
 
-    void Update()
-    {
-        
+    void DecaySanity(){
+        _mentalSanity -= _percentLossPerSecond;
+    }
+
+    void UpdateDisplay(){
+        _imgSanityJauge.fillAmount = _mentalSanity / 100;
+
+        float t = 1.0f - (_mentalSanity/100.0f);
+        Debug.Log(t);
+        float alpha = Mathf.Lerp(0, 1, t );
+        Color c = new Color(0, 0, 0, alpha);
+        Debug.Log(c);
+
+
+        _imgPanelSanity.color = c;
     }
 }
